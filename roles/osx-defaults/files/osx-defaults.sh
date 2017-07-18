cc5e5d327625e4e8fa9b30ae5dd5335fc70c69af
Clint Plummer <clint@yuhonas.com>
2017-07-18 08:53:25 +0300

    Comment out/remove broken packages + Add Mac OS Defaults

---
+++ roles/osx-defaults/files/osx-defaults.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# References
+# ----------------
+# https://github.com/donnemartin/dev-setup/blob/master/osx.sh
+# https://github.com/herrbischoff/awesome-osx-command-line/blob/master/README.md
+
+# All controls
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Show Full Path in Finder Title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Set current Folder as Default Search Scope
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+
+# Bottom left screen corner → Start screen saver
+# defaults write com.apple.dock wvous-bl-corner -int 5
+# defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# Always show scrollbars
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+# Possible values: `WhenScrolling`, `Automatic` and `Always`
