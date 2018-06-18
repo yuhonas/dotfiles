0eb5eda1d36e1c1a863e09bc7a94d6083aea46b5
Clint Plummer <clint@yuhonas.com>
2018-06-18 21:01:21 +1000

    add neomutt + fish aliases & config

--- roles/dotfiles/files/Mackup/.config/fish/config.fish
+++ roles/dotfiles/files/Mackup/.config/fish/config.fish
@@ -1,8 +1,33 @@
-# use exa over standard ls
-function ls
-  exa $argv
-end
+# ls aliases
+alias ls=exa
 
-function ll
-  exa -al $argv
-end
+
+# git aliases
+alias g=git
+alias gco="git checkout"
+alias gb="git branch"
+alias gst="git status"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias gc="git commit --verbose"
+
+# diff
+alias diffu="colordiff --unified"
+alias diff="colordiff"
+
+# use sublime as the default editor
+
+export EDITOR="subl -w"
+export VISUAL="subl -w"
+
+# lesspipe
+
+export LESSOPEN="|/usr/local/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
+
+# get the weather
+
+alias weather="curl wttr.in/Melbourne"
+
+# use rbevnv shims over local system ruby
+
+export PATH="$HOME/.rbenv/shims:$PATH"
