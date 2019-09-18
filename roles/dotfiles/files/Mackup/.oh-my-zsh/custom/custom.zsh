a9ff03b0c8a99b2bc690fe14c7b5252d5e0a5892
Clint Plummer <clint@yuhonas.com>
2019-09-18 09:26:00 -0700

    Add helpful commentary to customizations

--- roles/dotfiles/files/Mackup/.oh-my-zsh/custom/custom.zsh
+++ roles/dotfiles/files/Mackup/.oh-my-zsh/custom/custom.zsh
@@ -9,13 +9,11 @@ export FZF_DEFAULT_OPTS="--reverse
 --preview='less -10 {+} 2>/dev/null'"
 
 # use vim as the default editor
-
 export VISUAL=vim
 export EDITOR="$VISUAL"
 export TERM=xterm-256color
 
 # heroku aliases
-
 alias h="heroku"
 alias hl="heroku logs -t"
 alias hc="heroku run console"
@@ -24,13 +22,16 @@ alias hp="heroku ps"
 alias hnr="heroku addons:open newrelic"
 alias be="bundle exec"
 
+# color setup for ls
+# http://man7.org/linux/man-pages/man1/dircolors.1.html        
 eval $(gdircolors ~/.dircolors/dircolors.256dark)
 
-bindkey -s "^[l" "exa -al^J" # shortcut to listing a directory
-bindkey -s "^[r" "ranger^J" # shortcut to file explorer
+# custom keybindings for fast directory exploration
+bindkey -s "^[l" "exa -al^J" # alt-l shortcut to listing a directory
+bindkey -s "^[r" "ranger^J"  # alt-r shortcut to file explorer
 
-# lesspipe
-
+# less input pre-processing through lesspipe
+# https://manpages.debian.org/jessie/less/lesspipe.1.en.html
 export LESSOPEN="|/usr/local/bin/lesspipe.sh %s"
 LESS_ADVANCED_PREPROCESSOR=1
 
@@ -39,7 +40,6 @@ LESS_ADVANCED_PREPROCESSOR=1
 alias cdu='cd-gitroot'
 
 # get the weather
-
 alias weather="curl wttr.in/Melbourne"
 
 # Setup frequent dirs in the CDPATH so we can cd to them from anywhere
@@ -47,7 +47,7 @@ alias weather="curl wttr.in/Melbourne"
 setopt auto_cd
 cdpath=($HOME/Sites $HOME/src)
 
-# Setup a suffix alias to allow fast editing using just the filename
+# Setup a zsh suffix alias to allow fast editing using just the filename
 # https://opensource.com/article/18/9/tips-productivity-zsh
 alias -s {rb,go,py,js,txt,md,yaml,yml}=$EDITOR
 
