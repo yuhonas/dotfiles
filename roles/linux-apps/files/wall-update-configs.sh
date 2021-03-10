2a025df30bc5f5dd3b6459f3af93407fdcf7ec5e
Clint Plummer <clint@yuhonas.com>
2021-03-10 16:16:49 +0800

    Add custom config's for linux ricing

---
+++ roles/linux-apps/files/wall-update-configs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# use a rofi dark theme if pywall has generated one
+WALL_ROFI_THEME="$HOME/.cache/wal/colors-rofi-dark.rasi"
+ROFI_DIR=$HOME/.config/rofi
+
+WALL_WAYBAR_THEME="$HOME/.cache/wal/colors-waybar.css"
+WAYBAR_DIR=$HOME/.config/waybar
+
+if [[ -f $WALL_ROFI_THEME ]]; then
+  [ -d $ROFI_DIR ] || mkdir -p $ROFI_DIR
+  cp -v $WALL_ROFI_THEME $ROFI_DIR/config.rasi
+fi
+
+# load theme into env variables / also themes FZF
+# see https://github.com/dylanaraps/pywal/wiki/Customization#user-content-scripting-files
+. $HOME/.cache/wal/colors.sh
+
+# add custom css for waybar
+if [[ -f $WALL_WAYBAR_THEME ]]; then
+  [ -d $WAYBAR_DIR ] || mkdir -p $WAYBAR_DIR
+  cp -v $WALL_WAYBAR_THEME $WAYBAR_DIR/custom.css
+fi
+
+# reload sway if we have it
+if type swaymsg > /dev/null 2>&1; then
+  swaymsg reload
+fi
