fcbf86b846a26df8f6ec219bf90b5f5bcdda2fd1
Clint Plummer <clint@yuhonas.com>
2024-01-04 05:43:57 +1100

    fix wallpaper selector as it was broken in sonora + bump node version

--- roles/dotfiles/files/Mackup/.local/bin/select-wallpaper.sh
+++ roles/dotfiles/files/Mackup/.local/bin/select-wallpaper.sh
@@ -9,14 +9,24 @@
 
 wallpaper_file=$(osascript -e 'tell application (path to frontmost application as text)
   try
-    set myFile to choose file with prompt "Select Wallpaper:" of type "public.image"
-    POSIX path of myFile
+  set imageFile to (choose file with prompt "Select an image file:" of type "public.image") as text
   end try
-end tell')
+end tell
 
+tell application "System Events"
+	repeat with desktopIndex from 1 to count of desktops
+		tell desktop desktopIndex
+			set picture to imageFile
+		end tell
+	end repeat
+end tell
+
+return POSIX path of imageFile')
 
 if [ -n "$wallpaper_file" ]; then
-  if wal -i "$wallpaper_file" -q; then
+  # do not set the walpaper using wal, it's broken in macos sonora
+  # see https://github.com/dylanaraps/pywal/issues/715
+  if wal -n -i "$wallpaper_file" -q; then
     accent_color=$(jq --raw-output '.colors.color9' < "$HOME/.cache/wal/colors.json")
 
     echo "Found color $accent_color"
